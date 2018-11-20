c1553ad13d778ff8b6b7519d466d7886f463cb2a
Rachel A St Clair <rachel.aileen.st@gmail.com>
2018-11-20 13:05:23 -0500

    first initialize

---
+++ C.eVICHE.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 12:35:08 2018
+
+@author: mpcr
+"""
+
+###C.eVICHE###
+##C.elegan Visual Implementation in Computer Heuristic Experiments##
+
+import deeplabcut
+
+
+#create the project, set working directory for videos, and find videos
+deeplabcut.create_new_project('CeVICHE','Rachel_StClair', ['Full path of video 1'], working_directory='/home/mpcr/Desktop/Rachel/CeVICHE',copy_videos=False)
+#find more videos
+deeplabcut.add_new_videos(`Full path of the project configuration file*',[`full path of video 4', `full path of video 5'],copy_videos=True/False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
