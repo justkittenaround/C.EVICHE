2fb306e8d36050f8dd2dc6114bfb55ae84f97724
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-02-14 13:13:56 -0500

    Add files via upload

--- C.eVICHE.py
+++ C.eVICHE.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 20 12:35:08 2018
-
 @author: Rachel St Clair
 """
 
@@ -12,54 +11,59 @@ Created on Tue Nov 20 12:35:08 2018
 
 
 import cv2
+import numpy as np
+import os
 
-vid = '/home/default/Downloads/DJI_0009 Shark cut 10.avi'
-cap = cv2.VideoCapture(vid)
-ret, frame = cap.read()
-frame.shape
-###
+folders = '/home/whale/Desktop/Rachel/CeVICHE/10-26-worms/avi/', '/home/whale/Desktop/Rachel/CeVICHE/11-14 WORMS', '/home/whale/Desktop/Rachel/CeVICHE/11-17-WORMS/avi/', '/home/whale/Desktop/Rachel/CeVICHE/11-21-worms/avi/'
 
-vid = '/home/default/Downloads/DJI_0009 Shark cut 10.avi'
-cap = cv2.VideoCapture(vid)
-cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('frame', 800,800)
-while(cap.isOpened()):
+videos = []
+for folder in folders:
+  fin = len(os.listdir(folder))
+  for n in range(1, fin):
+    if n < 10:
+     n = '00' + str(n)
+    else:
+     n = '0' + str(n)
+    folder = folder + n + '/'
+    for vid in os.listdir(folder):
+      videos.append(vid)
+      print(vid)
+
+
+
+
+def get_frames(videos)
+  #which video
+  vid = '/home/whale/Desktop/Rachel/CeVICHE/10-26-worms/avi/001/10-26-worms-001.avi'
+
+  # Create a VideoCapture object and read from input file
+  # If the input is the camera, pass 0 instead of the video file name
+  cap = cv2.VideoCapture(vid)
+
+  # Read until video is completed
+  while(cap.isOpened()):
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if ret == True:
+ 
+      # Display the resulting frame
+      #cv2.imshow('Frame',frame)
+ 
+      # Press Q on keyboard to  exit
+      #if cv2.waitKey(25) & 0xFF == ord('q'):
+        #break
+ 
+    # Break the loop
+    else: 
+      break
 
-cap.release()
-cv2.destroyAllWindows()
-##
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    print(frame.shape)
+  # When everything done, release the capture
+  #cap.release()
+  #cv2.destroyAllWindows()
 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return frame
+##frame transformations
+frame = frame.mean(axis=-1,keepdims=1)
 
 
 
