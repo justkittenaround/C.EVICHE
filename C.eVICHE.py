918b299dedc6c1a33ac07dce6291fc8fd5ffe5fc
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-02-18 17:05:35 -0500

    Add files via upload

--- C.eVICHE.py
+++ C.eVICHE.py
@@ -13,57 +13,31 @@ Created on Tue Nov 20 12:35:08 2018
 import cv2
 import numpy as np
 import os
+from glob import glob
 
-folders = '/home/whale/Desktop/Rachel/CeVICHE/10-26-worms/avi/', '/home/whale/Desktop/Rachel/CeVICHE/11-14 WORMS', '/home/whale/Desktop/Rachel/CeVICHE/11-17-WORMS/avi/', '/home/whale/Desktop/Rachel/CeVICHE/11-21-worms/avi/'
+#vid = '/home/whale/Desktop/Rachel/CeVICHE/10-26-worms/avi/001/10-26-worms-001.avi'
 
-videos = []
-for folder in folders:
-  fin = len(os.listdir(folder))
-  for n in range(1, fin):
-    if n < 10:
-     n = '00' + str(n)
-    else:
-     n = '0' + str(n)
-    folder = folder + n + '/'
-    for vid in os.listdir(folder):
-      videos.append(vid)
-      print(vid)
+##
+os.chdir('/home/whale/Desktop/Rachel/CeVICHE/')
+names = glob('**/*/*.avi', recursive=True)
 
-
-
-
-def get_frames(videos)
-  #which video
-  vid = '/home/whale/Desktop/Rachel/CeVICHE/10-26-worms/avi/001/10-26-worms-001.avi'
-
-  # Create a VideoCapture object and read from input file
-  # If the input is the camera, pass 0 instead of the video file name
+def get_frames(vid):
   cap = cv2.VideoCapture(vid)
-
-  # Read until video is completed
-  while(cap.isOpened()):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret == True:
- 
-      # Display the resulting frame
-      #cv2.imshow('Frame',frame)
- 
-      # Press Q on keyboard to  exit
-      #if cv2.waitKey(25) & 0xFF == ord('q'):
-        #break
- 
-    # Break the loop
-    else: 
-      break
-
-  # When everything done, release the capture
-  #cap.release()
-  #cv2.destroyAllWindows()
-
+  ret, frame = cap.read()
   return frame
-##frame transformations
-frame = frame.mean(axis=-1,keepdims=1)
 
+if len(names) == 166:
+  del(names[113])
+else:
+  pass
 
+def scrape(names):
+  data = np.zeros([len(names), 224, 256, 1])
+  for idx, vid in enumerate(names):
+    frames = get_frames(vid)
+    frames = frames.mean(axis=-1, keepdims=1)
+    frames = np.resize(frames, (224, 256, 1))
+    data[idx, ...] = frames
+  return data
 
+data = scrape(names)
