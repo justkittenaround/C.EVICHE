3fa5207132bd98ac35103437ff2bf3a461dc8ed8
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-02-21 20:12:08 -0500

    Add files via upload

--- C.eVICHE.py
+++ C.eVICHE.py
@@ -17,10 +17,18 @@ import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
 os.environ["CUDA_VISIBLE_DEVICES"]="0";
 
+import tensorflow as tf
 import keras
-from keras.applications import resnet50
+from keras.applications.resnet50 import ResNet50
 from keras.models import Sequential
+from keras.layers import Flatten
+from keras.layers import LSTM
+from keras.layers import TimeDistributed
+from keras.layers import Embedding
 from keras.layers import ConvLSTM2D
+from keras.layers import Dense
+from keras import optimizers
+from keras import backend as K
 
 label_file = '/home/whale/Desktop/Rachel/CeVICHE/Time2Seize - Sheet1 (1).csv'
 vid = '10-26-worms/avi/015/10-26-worms-015.avi'
@@ -77,6 +85,8 @@ x, y = get_hot(vid)
 x = x.astype('float32')
 x_mean = np.mean(x, 0)
 x -= x_mean
+x = tf.convert_to_tensor(x)
+y = tf.convert_to_tensor(y)
 
 ##model
 #hyperparameters
@@ -88,19 +98,24 @@ loss = 'categorical_crossentropy'
 dialation_rate = (3, 3)
 
 ##model
+res = Sequential()
+res.add(ResNet50(weights='imagenet', include_top=False, input_shape=(256,256,3)))
+res.add(keras.layers.Embedding(4, 5))
+res.add(keras.layers.Flatten(data_format=None))
+# model.add(keras.layers.MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 model = Sequential()
-model.add(resnet50.ResNet50(weights='imagenet', include_top=False, input_shape=(256,256,3)))
-model.add(keras.layers.ConvLSTM2D(512,(3,3), strides=(1, 1), dilation_rate=dialation_rate, go_backwards=False)
-model.add(keras.layers.ConvLSTM2D(512,(3,3), strides=(1, 1), dilation_rate=dialation_rate, go_backwards=True)
-# model.add(keras.layers.LSTM(2, activation=activation, recurrent_activation=activation)
+model.add(keras.layers.TimeDistributed(res))
+model.add(keras.layers.ConvLSTM2D(5,(1,1), strides=(1, 1), dilation_rate=dialation_rate, go_backwards=False))
+model.add(keras.layers.ConvLSTM2D(5,(1,1), strides=(1, 1), dilation_rate=dialation_rate, go_backwards=True))
 model.add(keras.layers.GlobalAveragePooling2D(data_format=None))
-model.add(Dense(1, activation=(keras.activations.softmax(x, axis=-1))))
+model.add(keras.layers.Dense(1, activation='relu'))
 
 model.compile(loss= loss,
-              optimizer=‘adam’,
+              optimizer=keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),
               metrics=['accuracy'])
 
 ##train
+
 model.fit(x, y, epochs=epochs, batch_size=batch_size, shuffle=False)
 
 ##evaluate
