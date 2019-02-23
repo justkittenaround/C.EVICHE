7a43550611ce77b88e3f5a170cd437da11469698
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-02-23 16:48:09 -0500

    Add files via upload

--- C.eVICHE.py
+++ C.eVICHE.py
@@ -1,15 +1,11 @@
 
-
 import cv2
 import numpy as np
 import os
 from glob import glob
-import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
-os.environ["CUDA_VISIBLE_DEVICES"]="0";
-
 import tensorflow as tf
 import keras
+from keras.utils import multi_gpu_model
 from keras.applications.resnet50 import ResNet50
 from keras.models import Sequential
 from keras.layers import Flatten
@@ -22,6 +18,12 @@ from keras.layers import Activation
 from keras import losses
 from keras import optimizers
 from keras import backend as K
+import skimage
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
+# os.environ["CUDA_VISIBLE_DEVICES"]="0";
+
+
+
 
 label_file = '/home/whale/Desktop/Rachel/CeVICHE/Time2Seize - Sheet1 (2).csv'
 
@@ -39,24 +41,31 @@ def get_test():
 train_names, train_labels = get_train()
 # test_names = get_test()
 
+n = 2000
+
 def get_frames(vid):
     cap = cv2.VideoCapture(vid)
     fps = int(cap.get(5))
     total_frames = int(round(cap.get(7)))
-    all_frames = np.asarray([])
+    print(total_frames, vid)
+    all_frames = np.zeros([n,224,256,3])
+    count = 0
+    ind = np.random.choice(total_frames, n, replace=False)
     while(True):
          ret, frame = cap.read()
-         idx = int(round(cap.get(1)))
-         frame = np.resize(frame, (idx, 256, 256, 3))
-         all_frames = np.concatenate([frame for idx in range(total_frames)], axis=0)
+         frame = skimage.transform.resize(frame, (224,256,3))
+         print(frame.shape)
+         if count in ind:
+            all_frames[count, ...] = frame
+         count += 1
          if cv2.waitKey(1):
             break
     cap.release()
     cv2.destroyAllWindows()
-    return(all_frames, total_frames)
+    return(all_frames, total_frames, ind)
 
 def get_hot(vid, names, labels):
-    get, cols = get_frames(vid)
+    get, total_frames, ind = get_frames(vid)
     row = names.index(vid)
     seizing_worms = int(labels[row, 13])
     bubble = int(labels[row, 14])
@@ -67,15 +76,16 @@ def get_hot(vid, names, labels):
     w5 = labels[row, 19]
     w6 = labels[row, -1]
     worms = [int(w1), int(w2), int(w3), int(w4), int(w5), int(w6)]
-    hot = np.zeros((6, cols))
+    hot = np.zeros((6, total_frames))
     for idx, worm in enumerate(worms):
-        for n in range(bubble, worm):
-            hot[idx, n] = 1
+        for i in range(bubble, worm):
+            if i  in ind:
+                hot[idx, i] = 1
+    hot = hot[:, ind]
     seize_time_frame = np.sum(hot,axis=0)
+
     if seizing_worms > 0:
         seize_time_frame = seize_time_frame/seizing_worms
-    else:
-        pass
     return(get, seize_time_frame)
 
 def trans(x):
@@ -85,51 +95,44 @@ def trans(x):
     return(x)
 
 epochs = 1
-batch_size = 3
+batch_size = 30
 opt = 'adam'
 act = 'elu'
-soft = 'softmax'
+soft = ''
 loss = 'mean_squared_error'
 dialation_rate = (1, 1)
 units = 100
 
 def create_network():
     res = Sequential()
-    res.add(ResNet50(weights='imagenet', include_top=False, input_shape=(256,256,3)))
-    res.add(keras.layers.GlobalMaxPooling2D(data_format=None))
-    res.add(keras.layers.Dense((1), activation=soft))
+    res.add(ResNet50(weights='imagenet', include_top=False, input_shape=(224,256,3)))
+    res.add(keras.layers.GlobalAveragePooling2D(data_format=None))
+    # res.add(keras.layers.Reshape((batch_size, 2048, 1, 1, 1))
+    # res.add(keras.layers.ConvLSTM2D(64, (3,3), strides=(1, 1), dilation_rate=(1, 1), activation='tanh', recurrent_activation='hard_sigmoid', return_sequences=True, go_backwards=False)))
+    # res.add(keras.layers.ConvLSTM2D(64, (3,3), strides=(1, 1), dilation_rate=(1, 1), activation='tanh', recurrent_activation='hard_sigmoid', return_sequences=True, go_backwards=False)))
+    res.add(keras.layers.Dense(1, activation='linear'))
+    # parallel_res = multi_gpu_model(res, gpus=2)
+    # parallel_res.compile(loss=loss,optimizer=opt)
     res.compile(loss=loss,optimizer=opt)
-    lstm = Sequential()
-    lstm.add(keras.layers.LSTM(units, activation='tanh', recurrent_activation='hard_sigmoid', go_backwards=False))
-    lstm.add(keras.layers.LSTM(units, activation='tanh', recurrent_activation='hard_sigmoid', go_backwards=True))
-    lstm.add(keras.layers.Dense(1, activation=soft))
-    lstm.compile(loss=loss,optimizer=opt)
-    return(res, lstm)
+    return(res)
 
 def run_it(train_vid):
     x_train, y_train = get_hot(train_vid, train_names, train_labels)
-    # x_test = get_frames(test_vid)
     x_train = trans(x_train)
+    # x_test = get_frames(test_vid)
     # x_test = trans(x_test)
-
-    res, lstm = create_network()
-
-    res.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, shuffle=False)
-    score = res.evaluate(x_train, y_train, batch_size=batch_size)
-    classes = res.predict(x_train, batch_size=batch_size)
-    classes = np.asarrray(classes)
-    classes = np.resize(classes, (batch_size, 1, 1))
-
-    model = lstm.fit(classes, y_train, epochs=epochs, batch_size=batch_size, shuffle=False)
-    score = lstm.evaluate(classes, y_train, batch_size=batch_size)
-    # final = lstm.predict(x_test, batch_size=batch_size)
-    return(final, model, score)
+    res = create_network()
+    final = res.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, shuffle=False)
+    # score = res.evaluate(x_train, y_train, batch_size=batch_size)
+    # final = res.predict(x_test, batch_size=batch_size)
+    return(final)
 
 def train_loop():
     win = np.asarray([])
     for train_vid in train_names:
         final = run_it(train_vid)
-        win = np.concatenate([final], axis=0)
-    return(win, model, score)
+        # win = np.concatenate([final], axis=0)
+    return(win, score)
 
-win, model, score = train_loop()
+win, score = train_loop()
+# res.save
