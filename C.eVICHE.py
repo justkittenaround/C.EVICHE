4e40a56d97a54d997a4da544d22d7e250451091f
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-02-22 19:38:01 -0500

    Add files via upload

--- C.eVICHE.py
+++ C.eVICHE.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 20 12:35:08 2018
-@author: Rachel St Clair
-"""
 
-###C.eVICHE###
-##C.elegan Visual Implementation in Computer Heuristic Experiments##
-##############
 
 import cv2
 import numpy as np
@@ -27,15 +18,26 @@ from keras.layers import TimeDistributed
 from keras.layers import Embedding
 from keras.layers import ConvLSTM2D
 from keras.layers import Dense
+from keras.layers import Activation
+from keras import losses
 from keras import optimizers
 from keras import backend as K
 
-label_file = '/home/default/C.EVICHE/Time2Seize - Sheet1 (1).csv'
-vid = '/home/default/C.EVICHE/train/10-26-worms/avi/001/10-26-worms-001.avi'
+label_file = '/home/whale/Desktop/Rachel/CeVICHE/Time2Seize - Sheet1 (2).csv'
 
-os.chdir('/home/default/C.EVICHE/train')
-names = glob('**/*/*.avi', recursive=True)
-labels = np.genfromtxt(label_file, delimiter=',', skip_header=1, usecols=range(0,20))
+def get_train():
+    os.chdir('/home/whale/Desktop/Rachel/CeVICHE/train')
+    train_names = sorted(glob('**/*/*.avi', recursive=True))
+    train_labels = np.genfromtxt(label_file, delimiter=',', skip_header=2, usecols=range(0,20))
+    return(train_names, train_labels)
+
+def get_test():
+    os.chdir('/home/whale/Desktop/Rachel/CeVICHE/test')
+    test_names = sorted(glob('**/*/*.avi', recursive=True))
+    return(test_names)
+
+train_names, train_labels = get_train()
+# test_names = get_test()
 
 def get_frames(vid):
     cap = cv2.VideoCapture(vid)
@@ -45,7 +47,6 @@ def get_frames(vid):
     while(True):
          ret, frame = cap.read()
          idx = int(round(cap.get(1)))
-         frame = frame.resize(256,256,3)
          frame = np.resize(frame, (idx, 256, 256, 3))
          all_frames = np.concatenate([frame for idx in range(total_frames)], axis=0)
          if cv2.waitKey(1):
@@ -54,74 +55,81 @@ def get_frames(vid):
     cv2.destroyAllWindows()
     return(all_frames, total_frames)
 
-def get_hot(vid):
+def get_hot(vid, names, labels):
     get, cols = get_frames(vid)
     row = names.index(vid)
-    bubble = int(labels[row, 13])
-    w1 = labels[row, 14]
-    w2 = labels[row, 15]
-    w3 = labels[row, 16]
-    w4 = labels[row, 17]
-    w5 = labels[row, 18]
-    w6 = labels[row, 19]
+    seizing_worms = int(labels[row, 13])
+    bubble = int(labels[row, 14])
+    w1 = labels[row, 15]
+    w2 = labels[row, 16]
+    w3 = labels[row, 17]
+    w4 = labels[row, 18]
+    w5 = labels[row, 19]
+    w6 = labels[row, -1]
     worms = [int(w1), int(w2), int(w3), int(w4), int(w5), int(w6)]
-    seizing_worms = int(labels[row, 12])
     hot = np.zeros((6, cols))
     for idx, worm in enumerate(worms):
-        duration = bubble - worm
-        for n in range(duration):
-            hot = np.put(hot, [idx, n], 3)
+        for n in range(bubble, worm):
+            hot[idx, n] = 1
     seize_time_frame = np.sum(hot,axis=0)
-    if seizing_worms == 0:
-        pass
     if seizing_worms > 0:
         seize_time_frame = seize_time_frame/seizing_worms
+    else:
+        pass
     return(get, seize_time_frame)
 
-x, y = get_hot(vid)
+def trans(x):
+    x_mean = np.mean(x, 0)
+    x_mean = x_mean.astype('uint8', copy=False)
+    x -= x_mean
+    return(x)
 
-#transformations
-x = x.astype('float32')
-x_mean = np.mean(x, 0)
-x -= x_mean
-x = tf.convert_to_tensor(x)
-y = tf.convert_to_tensor(y)
-
-##model
-#hyperparameters
-epochs = 3
+epochs = 1
 batch_size = 3
-optimizer = 'adam'
-act = 'tanh'
-loss = 'categorical_crossentropy'
-dialation_rate = (3, 3)
+opt = 'adam'
+act = 'elu'
+soft = 'softmax'
+loss = 'mean_squared_error'
+dialation_rate = (1, 1)
+units = 100
 
-##model
 def create_network():
     res = Sequential()
     res.add(ResNet50(weights='imagenet', include_top=False, input_shape=(256,256,3)))
-    res.add(keras.layers.Embedding(4, 5))
-    res.add(keras.layers.Flatten(data_format=None))
-    # model.add(keras.layers.MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
-    model = Sequential()
-    model.add(keras.layers.TimeDistributed(res))
-    model.add(keras.layers.ConvLSTM2D(5,(1,1), strides=(1, 1), dilation_rate=dialation_rate, go_backwards=False))
-    model.add(keras.layers.ConvLSTM2D(5,(1,1), strides=(1, 1), dilation_rate=dialation_rate, go_backwards=True))
-    model.add(keras.layers.GlobalAveragePooling2D(data_format=None))
-    model.add(keras.layers.Dense(1, activation='sigmoid'))
-    model.compile(loss= loss,optimizer=keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),metrics=['accuracy'])
-    return model
-network_model = create_network()
-model.fit(x, y, epochs=epochs, batch_size=batch_size, shuffle=False)
+    res.add(keras.layers.GlobalMaxPooling2D(data_format=None))
+    res.add(keras.layers.Dense((1), activation=soft))
+    res.compile(loss=loss,optimizer=opt)
+    lstm = Sequential()
+    lstm.add(keras.layers.LSTM(units, activation='tanh', recurrent_activation='hard_sigmoid', go_backwards=False))
+    lstm.add(keras.layers.LSTM(units, activation='tanh', recurrent_activation='hard_sigmoid', go_backwards=True))
+    lstm.add(keras.layers.Dense(1, activation=soft))
+    lstm.compile(loss=loss,optimizer=opt)
+    return(res, lstm)
 
+def run_it(train_vid):
+    x_train, y_train = get_hot(train_vid, train_names, train_labels)
+    # x_test = get_frames(test_vid)
+    x_train = trans(x_train)
+    # x_test = trans(x_test)
 
-x = np.zeros((618,256,256,3))
-y = np.zeros((618))
+    res, lstm = create_network()
 
+    res.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, shuffle=False)
+    score = res.evaluate(x_train, y_train, batch_size=batch_size)
+    classes = res.predict(x_train, batch_size=batch_size)
+    classes = np.asarrray(classes)
+    classes = np.resize(classes, (batch_size, 1, 1))
 
+    model = lstm.fit(classes, y_train, epochs=epochs, batch_size=batch_size, shuffle=False)
+    score = lstm.evaluate(classes, y_train, batch_size=batch_size)
+    # final = lstm.predict(x_test, batch_size=batch_size)
+    return(final, model, score)
 
-##evaluate
-# score = model.evaluate(x_test, y_test, batch_size=batch_size)
+def train_loop():
+    win = np.asarray([])
+    for train_vid in train_names:
+        final = run_it(train_vid)
+        win = np.concatenate([final], axis=0)
+    return(win, model, score)
 
-##test
-# classes = model.predict(x_test, batch_size=batch_size)
+win, model, score = train_loop()
