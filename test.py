c8e6dbae2adb3b35dbd0cb1e9ba78736dbb18ca3
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-05-16 18:50:28 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -9,16 +9,16 @@ import visdom
 vis = visdom.Visdom()
 
 PATH = '/home/whale/Desktop/Rachel/CeVICHE/models/modelsvgg.pt'
-data_dir = '/home/whale/Desktop/Rachel/CeVICHE/Data/'
+data_dir = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data'
 batch_size = 1
 num_classes = 7
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 input_size = 224
-phase = 'val'
+phase = 'train'
 
 ##DATASETS AND DATALOADERS################################################
 data_transforms = {
-    'val': transforms.Compose([
+    'train': transforms.Compose([
         transforms.Resize(input_size),
         transforms.CenterCrop(input_size),
         transforms.ToTensor(),
@@ -34,19 +34,29 @@ def testing_model(model, dataloaders):
     since = time.time()
 
     worms_seizing_pred = [np.array([7])]
+    worms_seizing_smoothed = [np.array([7])]
     worms_seizing_act = [np.array([7])]
     testing_acc = []
     confusion = np.zeros([8, 8])
     running_corrects = 0
+    count = 2
 
     for inputs, labels in dataloaders[phase]:
         inputs = inputs.to(device)
         labels = labels.to(device)
         outputs = test_model(inputs)
-        _, preds = torch.max(outputs, 1)
-        running_corrects += torch.sum(preds == labels.data)
-        worms_seizing_pred.append(preds.cpu().numpy())
+        _, predicts = torch.max(outputs, 1)
+        running_corrects += torch.sum(predicts == labels.data)
+        preds = predicts.cpu().numpy()
+        worms_seizing_pred.append(preds)
         worms_seizing_act.append(labels.data.cpu().numpy())
+        seizing_preds = np.asarray(worms_seizing_pred)
+        print(count, len(seizing_preds))
+        if len(seizing_preds) == count and :
+            max = np.amax(seizing_preds[count:])
+            worms_seizing_smoothed.append(max)
+            count += 5
+        vis.line(worms_seizing_smoothed, win='worms_seizing_smoothed', opts=dict(title= 'smoothed_predictions'))
         vis.line(worms_seizing_pred, win='worms_seizing_predict', opts=dict(title= 'trainedVGG-Predictions'))
         vis.line(worms_seizing_act, win='worms_seizing_target', opts=dict(title= 'trainedVGG-Target'))
         confusion[worms_seizing_pred[-1], worms_seizing_act[-1]] += 1
@@ -54,7 +64,7 @@ def testing_model(model, dataloaders):
     testing_acc = (running_corrects/len(labels.data))
     time_elapsed = time.time() - since
     print('Testing complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Testing_acc: {:4f}', testing_acc)
+    print('Testing_acc:', testing_acc.cpu().numpy())
     return test_model, testing_acc
 
 ## INITIALIZE AND RESHAPE MODEL################################################
