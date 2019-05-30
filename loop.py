414ec62339aefe39c820d042d6542f4fb39d4099
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-05-30 13:11:24 -0400

    Add files via upload

--- loop.py
+++ loop.py
@@ -70,16 +70,7 @@ for target in sorted(files):
     count_2 = thresh
     idx = 0
     t = 0
-    one = 0
-    two = 0
-    thre = 0
-    tik_1 = 0
-    tik_2 = 0
-    tik_3 = 0
-    f = 0
-    a = np.zeros([total_frames])
-    b = np.zeros([total_frames])
-    c = np.zeros([total_frames])
+    check_type = np.array([1])
     p = np.asarray([0])
     print("Running Predictions...")
     since = time.time()
@@ -87,9 +78,9 @@ for target in sorted(files):
         s = time.time()
         cap.set(cv2.CAP_PROP_POS_FRAMES,t)
         ret, frame = cap.read()
-        if type(frame) is not type(a):
+        if type(frame) is not type(check_type):
             print('skipped', t)
-            break
+            continue
         frame = pil.fromarray(frame)
         frame = data_transforms(frame)
         frame = frame.unsqueeze(0)
@@ -99,7 +90,7 @@ for target in sorted(files):
         preds = predicts.cpu().numpy()
         seizing_preds = np.asarray(worms_seizing_pred)
         worms_seizing_pred.append(preds)
-        vis.line(worms_seizing_pred, win='worms_seizing_predict', opts=dict(title= 'trainedVGG_Predictions_' + str(round(fps/fps)) + 'sec'))
+        vis.line(worms_seizing_pred, win='worms_seizing_predict', opts=dict(title= 'Raw_Predictions_' + str(round(fps/fps)) + 'sec'))
 
         if len(seizing_preds) == count_1:
             leng_1 = seizing_preds[(count_1-thresh):]
@@ -107,10 +98,6 @@ for target in sorted(files):
             worms_seizing_smoothed_1.append(max_1)
             count_1 += thresh
             vis.line(worms_seizing_smoothed_1, win='worms_seizing_smoothed_1', opts=dict(title= 'smoothed_' + str(round(fps/len(leng_1))) + 'sec_block'))
-            for n in range(0,(len(leng_1)+1)):
-                a[tik_1] = worms_seizing_smoothed_1[one]
-                tik_1 += 1
-            one += 1
 
         if len(seizing_preds) == count_2:
             leng_2 = seizing_preds[idx:]
@@ -119,14 +106,6 @@ for target in sorted(files):
             vis.line(worms_seizing_smoothed_2, win='worms_seizing_smoothed_2', opts=dict(title= 'smoothed_' + str(round(fps/len(leng_2))) + 'sec_slide'))
             idx += 1
             count_2 += 1
-            if tik_2 <= len(leng_2):
-                for n in range(0,(len(leng_2)+1)):
-                    b[tik_2] = worms_seizing_smoothed_2[two]
-                    tik_2 += 1
-            else:
-                b[tik_2] = worms_seizing_smoothed_2[two]
-                tik_2 += 1
-            two += 1
 
         if idx >= (thresh*surround):
             num = thresh*surround
@@ -135,34 +114,13 @@ for target in sorted(files):
             max_3 = np.argmax(np.bincount(np.squeeze(leng_3)))
             worms_seizing_smoothed_3.append(max_3)
             vis.line(worms_seizing_smoothed_3, win='worms_seizing_smoothed_3', opts=dict(title= 'smoothed_' + str(round(len(leng_3)/fps)) + 'sec_surround'))
-            if tik_3 <= len(leng_3):
-                for n in range(0,(len(leng_3)+1)):
-                    c[tik_3] = worms_seizing_smoothed_3[thre]
-                    tik_3 += 1
-            else:
-                c[tik_3] = worms_seizing_smoothed_3[thre]
-                tik_3 += 1
-            thre += 1
 
+        p = time.time() - s
+        progress(t, total_frames, status=('predicting ' + str(t)))
+        time.sleep(p)
 
-        while f < one and thre > 1:
-        # while thre > 1 and f < len(worms_seizing_smoothed_1):
-            bin = np.append(worms_seizing_pred[f], [a[f]], axis=0)
-            bin = np.append(bin, [b[f]])
-            bin = np.append(bin, [c[f]])
-            print(len(worms_seizing_smoothed_1), f, bin)
-            max = np.argmax(np.bincount(np.squeeze(bin.astype(np.int64))))
-            worms_seizing_smoothed.append(max)
-            vis.line(worms_seizing_smoothed, win='worms_seizing_smoothed', opts=dict(title= 'smoothed_predictions_combined_' + str(round(fps/fps)) + 'sec'))
-            f += 1
-
-        # p = time.time() - s
-        # progress(t, total_frames, status=('predicting ' + str(t)))
-        # time.sleep(p)
-
-    print('lengths of time', leng_1, leng_2, leng_3)
-    print('smoothed1', len(worms_seizing_smoothed_1), 'smoothed2', len(worms_seizing_smoothed_2), 'smoothed3', len(worms_seizing_smoothed_3))
-    print('n', 'a.shape=', a.shape, 'b_shape=', b.shape, 'c_shape=', c.shape)
+    print('lengths of time', len(leng_1), len(leng_3))
+    print('smoothed1', len(worms_seizing_smoothed_1), 'smoothed3', len(worms_seizing_smoothed_3))
 
     time_elapsed = time.time() - since
     print('Testing completed in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60), 'for ' + str(target))
