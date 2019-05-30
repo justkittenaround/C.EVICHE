7b6c340933c21bcc0521cbabd2e3f87156aedddc
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-05-30 18:13:01 -0400

    Add files via upload

--- postproc.py
+++ postproc.py
@@ -12,17 +12,19 @@ import copy
 import cv2
 from PIL import Image as pil
 import visdom
-vis = visdom.Visdom()
-PATH = '/home/blu/C.EVICHE/saved_models/modelsvgg (1).pt'
-root = '/home/blu/C.EVICHE/data/test/'
-# PATH = '/home/whale/Desktop/Rachel/CeVICHE/models/modelsvgg.pt'
-# root = '/home/whale/Desktop/Rachel/CeVICHE/Data/test/'
+vis = visdom.Visdom(port=8090)
+# PATH = '/home/blu/C.EVICHE/saved_models/modelsvgg (1).pt'
+# root = '/home/blu/C.EVICHE/data/test/'
+PATH = '/home/whale/Desktop/Rachel/CeVICHE/models/modelsvgg.pt'
+root = '/home/whale/Desktop/Rachel/CeVICHE/Data/test/'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 input_size = 224
 thresh = 15
 surround = 6
+bubble_check = 120
 if thresh % 2 != 0:
     thresh += 1
+
 num = thresh*surround
 half = thresh/2
 half_num = num/2
@@ -64,9 +66,11 @@ for target in sorted(files):
     fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
     print('Total frames in ', target, ' is: ', total_frames)
     seizing_preds = []
-    smoothed =[]
+    inter = []
+    smoothed =[0]
     t = 0
     idx = 1
+    bubble = 0
     check_type = np.array([1])
     print("Running Predictions...")
     since = time.time()
@@ -87,37 +91,62 @@ for target in sorted(files):
         preds = int(preds)
         seizing_preds.append(preds)
         vis.line(seizing_preds, win='seizing_preds', opts=dict(title= 'Raw_Predictions'))
-        def correct_up(idx):
-            if len(seizing_preds[:idx]) > half:
-                max_15 = np.argmax(np.bincount(np.squeeze(seizing_preds[int(idx-half):int(idx+half+1)])))
+        def correct_15(idx, mode):
+            if len(mode[:idx]) > half:
+                max_15 = np.argmax(np.bincount(np.squeeze(mode[int(idx-half):int(idx+half+1)])))
             else:
-                length = len(seizing[:idx])
-                max_15 = np.argmax(np.bincount(np.squeeze(seizing_preds[int(idx-length):int(idx+half+1)])))
+                length = len(mode[:idx])
+                max_15 = np.argmax(np.bincount(np.squeeze(mode[int(idx-length):int(idx+half+1)])))
             return max_15
-        def correct_down(idx):
-            if len(seizing_preds[:idx]) > half_num:
-                max_90 = np.argmax(np.bincount(np.squeeze(seizing_preds[int(idx-half_num):int(idx+half_num+1)])))
+        def correct_90(idx):
+            if len(mode[:idx]) > half_num:
+                max_90 = np.argmax(np.bincount(np.squeeze(mode[int(idx-half_num):int(idx+half_num+1)])))
             else:
-                length = len(seizing[:idx])
-                max_90 = np.argmax(np.bincount(np.squeeze(seizing_preds[int(idx-length):int(idx+half_num+1)])))
+                length = len(mode[:idx])
+                max_90 = np.argmax(np.bincount(np.squeeze(mode[int(idx-length):int(idx+half_num+1)])))
             return max_90
-------------------------------------insert if >/< but also == to below------------------------------------------------
-        while len(seizing_preds[idx:]) > half_num:
-            if seizing_preds[idx] == seizing_preds[int(idx-1)] and seizing_preds[idx] == seizing_preds[int(idx+1)]:
-                smoothed.append(seizing_preds[idx])
-            elif seizing_preds[idx] > seizing_preds[int(idx-1)] and seizing_preds[idx] > seizing_preds[int(idx+1)]:
-                smoothed.append(correct_up(idx))
-            elif seizing_preds[idx] > seizing_preds[int(idx-1)] and seizing_preds[idx] < seizing_preds[int(idx+1)]:
-                smoothed.append(correct_up(idx))
-            elif seizing_preds[idx] < seizing_preds[int(idx-1)] and seizing_preds[idx] < seizing_preds[int(idx+1)]:
-                smoothed.append(correct_down(idx))
-            elif seizing_preds[idx] < seizing_preds[int(idx-1)] and seizing_preds[idx] > seizing_preds[int(idx+1)]:
-                smoothed.append(correct_down(idx))
+        def bubble(idx):
+            if len(inter[:idx]) > bubble_check:
+                max_bub = np.argmax(np.bincount(np.squeeze(inter[int(idx-bubble_check):int(idx+bubble_check+1)])))
             else:
-                smoothed.append(seizing_preds[idx])
-                print('missed scenario')
+                length = len(inter[:idx])
+                max_bub = np.argmax(np.bincount(np.squeeze(inter[int(idx-length):int(idx+bubble_check+1)])))
+            return max_bub
+#------------------------------------insert if >/< but also == to below------------------------------------------------
+        if len(seizing_preds) > bubble_check:
+            for idx in range(idx,bubble_check):
+                inter.append(correct_15(idx, seizing_preds))
+                vis.line(inter, win='inter_preds', opts=dict(title= 'Intermediate_Predictions'))
+            if len(inter) < bubble_check:
+                smoothed.append(inter[int(idx-2)])
+            if len(inter) > bubble_check and bubble(idx) < 0:
+                if seizing_preds[idx] > seizing_preds[int(idx-1)]:
+                    smoothed.append(correct_90(idx, seizing_preds))
+                    # print('1')
+                elif seizing_preds[idx] < seizing_preds[int(idx-1)]:
+                    smoothed.append(correct_15(idx, seizing_preds))
+                    # print('2')
+                elif seizing_preds[idx] > inter[int(idx-2)]:
+                    smoothed.append(correct_90(idx, inter))
+                    # print('3')
+                else:
+                    smoothed.append(inter[int(idx-1)])
+                vis.line(smoothed, win='smoothed_preds', opts=dict(title= 'Smoothed_Predictions'))
+            elif len(inter) > bubble_check and bubble(idx) > 0:
+                bubble = 1
+                if seizing_preds[idx] > seizing_preds[int(idx-1)]:
+                    smoothed.append(inter[int(idx-2)])
+                    # print('4')
+                elif seizing_preds[idx] < seizing_preds[int(idx-1)]:
+                    smoothed.append(inter[int(idx-2)])
+                    # print('5')
+                elif seizing_preds[idx] > inter[int(idx-2)]:
+                    smoothed.append(inter[int(idx-2)])
+                    # print('6')
+                else:
+                    smoothed.append(inter[int(idx-2)])
+                vis.line(smoothed, win='smoothed_preds', opts=dict(title= 'Smoothed_Predictions'))
             idx +=1
-            vis.line(smoothed, win='smoothed_preds', opts=dict(title= 'Smoothed_Predictions'))
         p = time.time() - s
         progress(t, total_frames, status=('predicting ' + str(t)))
         time.sleep(p)
