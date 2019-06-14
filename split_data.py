5c4746c3fb5ec63e76628e5bcc97edbeee21f35e
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-06-14 11:53:36 -0400

    Add files via upload

--- split_data.py
+++ split_data.py
@@ -1,44 +1,65 @@
-
-
 import os
 from glob import glob
 import numpy as np
 import shutil
 
+#specify locations##############################################################
+master = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data/'
+evaluate_path = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data/Evaluate'
+train_path = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data/train/'
+val_path = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data/val/'
 
-train_path = '/home/blu/C.EVICHE/data/conv_ceviche_data/train/storage/'
-val_path = '/home/blu/C.EVICHE/data/conv_ceviche_data/val/'
-storage = '/home/blu/C.EVICHE/data/conv_ceviche_data/storage/'
+#commands#######################################################################
+#make folders for [train, val]
+def make_folders():
+    for type in [train_path, val_path]:
+        folders = os.listdir(type)
+        for bit in ['0w', '1w', '2w', '3w', '4w', '5w', '6w']:
+            if bit not in folders:
+                os.mkdir(type + bit)
+make_folders()
 
-#split excess training data into storage and oversample underrepresented data###
-def overflow(folder):
-    os.chdir(train_path)
-    train = glob(folder + '/**.jpg')
-    amount = (len(train)-5783)
-    if amount > 0:
-        samples = np.random.choice(train, amount, replace=False)
-        for sample in samples:
-            src = train_path + sample
-            dst = storage + sample
-            shutil.move(src, dst)
-    elif amount < 0:
-        make = abs(amount)
-        samples = np.random.choice(train, make, replace=True)
-        for idx, file in enumerate(samples):
-            src = train_path + file
-            file = str(file)
-            name = file.split('/')
-            name = ('6w/copy' + str(idx) + name[1])
-            dst = train_path  + name
-            shutil.copyfile(src, dst)
+#combine '6w' imagefolders from multiple videos
+def combine():
+    os.chdir(evaluate_path)
+    fives = glob('*/5w/**.jpg')
+    r = 0
+    for vid in fives:
+        x = vid.split('/')
+        name = str(r) + x[-1]
+        r += 1
+        src = evaluate_path + '/' + vid
+        dst = train_path + '5w/' + name
+        shutil.copyfile(src, dst)
+    max5 = len(os.listdir(train_path + '5w/'))
+    max_all = len(os.listdir(evaluate_path))
+    dividens = int(round(max5/max_all))
+    return dividens, max5
 
-    print('finished modifying:', folder)
+dividens, max5 = combine()
 
-for bit in ['0w', '1w', '2w', '3w', '4w', '5w', '6w']:
-    overflow(bit)
+#copy training data into imagefolder
+def split(bit, max5):
+    os.chdir(evaluate_path)
+    train = glob('*/' + bit + '/**.jpg')
+    if len(train) >= max5:
+        samples = np.random.choice(train, max5, replace=False)
+    else:
+        samples = train
+    r = 0
+    for vid in samples:
+        x = vid.split('/')
+        name = str(r) + x[-1]
+        r += 1
+        src = evaluate_path + '/' + vid
+        dst = train_path + bit + name
+        shutil.copyfile(src, dst)
+    print('finished modifying:', bit)
 
+for bit in ['0w/', '1w/', '2w/', '3w/', '4w/', '6w/']:
+    split(bit, max5)
 
-#split training and validation into seperate folders############################
+#split training and validation into seperate folders
 def get_some(folder):
     os.chdir(train_path)
     train = glob(folder + '/**.jpg')
@@ -50,6 +71,23 @@ def get_some(folder):
         shutil.move(src, dst)
     print('finished moving:', folder)
 
-
 for bit in ['0w', '1w', '2w', '3w', '4w', '5w', '6w']:
-   get_some(bit)
+    get_some(bit)
+
+#oversample underrepresented dataset
+
+def oversample(max5):
+    amount = len(os.listdir(os.path.join(train_path, '6w/')))
+    make = int(abs(max5-amount))
+    os.chdir(train_path)
+    train = glob(bit + '/**.jpg')
+    samples = np.random.choice(train, make, replace=True)
+    for idx, file in enumerate(samples):
+        src = train_path + file
+        file = str(file)
+        name = file.split('/')
+        name = ('6w/copy' + str(idx) + name[1])
+        dst = train_path  + name
+        shutil.copyfile(src, dst)
+
+oversample(max5)
--- test.py
+++ test.py
@@ -7,17 +7,23 @@ from torchvision import transforms
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import cv2
+import h5py
+from glob import glob
+import skimage
+from scipy.misc import imsave, imresize
 import os
 import sys
 import copy
 import cv2
 from PIL import Image as pil
+from scipy import signal
+import csv
 import visdom
 vis = visdom.Visdom()
 
-label_file = '/home/whale/Desktop/Rachel/CeVICHE/Time2Seize - Sheet1 (3).csv'
-vid_folder = '/home/whale/Desktop/Rachel/CeVICHE/Data/Evaluate/'
-save_path = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data/test/Evaluate'
+label_file = '/home/whale/Desktop/Rachel/CeVICHE/Time2Seize - Sheet1 (3) - Time2Seize - Sheet1 (3).csv'
+vid_folder = '/home/whale/Desktop/Rachel/CeVICHE/conv_ceviche_data/Evaluate/'
 
 # PATH = '/home/blu/C.EVICHE/saved_models/modelsvgg (1).pt'
 PATH = '/home/whale/Desktop/Rachel/CeVICHE/models/modelsvgg.pt'
@@ -26,16 +32,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 batch_size = 1
 num_classes = 7
 input_size = 224
-phase = 'Evaluate'
-
-thresh = 15
-surround = 6
-bubble_check = 120
-if thresh % 2 != 0:
-    thresh += 1
-num = thresh*surround
-half = thresh/2
-half_num = num/2
+thresh = 61
+fps = 29
 
 ##progress bar##################################################################
 def progress(count, total, status=''):
@@ -46,92 +44,15 @@ def progress(count, total, status=''):
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush()
 
-##DATASETS AND DATALOADERS################################################
-def get_frames():
-    os.chdir(vid_folder)
-    train_names = sorted(glob('**/*/*.avi', recursive=True))
-    train_labels = np.genfromtxt(label_file, delimiter=',', skip_header=2, usecols=range(0,20))
-    print('labels:', train_labels.shape, 'names', len(train_names))
-
-    for vid in names:
-        cap = cv2.VideoCapture(vid)
-        total_frames = int(round(cap.get(7)))
-        fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
-        row = (names.index(vid))
-        seizing_worms = int(labels[row, 12])
-        bubble = int(labels[row, 13])
-        w1 = labels[row, 14]
-        w2 = labels[row, 15]
-        w3 = labels[row, 16]
-        w4 = labels[row, 17]
-        w5 = labels[row, 18]
-        w6 = labels[row, 19]
-        worms = [int(w1), int(w2), int(w3), int(w4), int(w5), int(w6)]
-        check = np.zeros((6, (total_frames+1)))
-        for idx, worm in enumerate(worms):
-            if worm == 0:
-                break
-            for frame_index in range(bubble, worm):
-                # print(frame_index)
-                check[idx, frame_index] = 1
-        worms_count = np.sum(check,axis=0)
-        for idx, num in enumerate(worms_count):
-            if idx >= 0 & idx <= total_frames:
-                cap.set(cv2.CAP_PROP_POS_FRAMES,idx)
-                ret, frame = cap.read()
-                # print(idx, type(frame))
-                if type(frame) is not type(worms_count):
-                    print('skipped', idx)
-                    break
-                # print(idx, num, frame.shape[2])
-                frame = imresize(frame, (224,224,3))
-                r = str(idx)
-                imsave(save_path + vid + r + '.jpg', frame)
-                if int(num) == 0:
-                    imsave(save_path + vid + '0w/'+ r + '.jpg', frame)
-                if int(num) == 1:
-                    imsave(save_path + vid + '1w/'+ r + '.jpg', frame)
-                if int(num) == 2:
-                    imsave(save_path + vid + '2w/'+ r + '.jpg', frame)
-                if int(num) == 3:
-                    imsave(save_path + vid + '3w/'+ r + '.jpg', frame)
-                if int(num) == 4:
-                    imsave(save_path + vid + '4w/'+ r + '.jpg', frame)
-                if int(num) == 5:
-                    imsave(save_path + vid + '5w/'+ r + '.jpg', frame)
-                if int(num) == 6:
-                    imsave(save_path + vid + '6w/'+ r + '.jpg', frame)
-        print('finished with vid:', vid)
-        return(train_names, train_labels, total_frames, fps)
-names, labels, total_frames, fps, vid = get_frames()
-
-data_transforms = {
-    phase: transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.CenterCrop(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-print("Initializing Datasets and Dataloaders...")
-image_datasets = {x: datasets.ImageFolder(os.path.join(save_path, x), data_transforms[x]) for x in [phase]}
-dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=False, num_workers=4) for x in [phase]}
-
 ##TESTING PROCEDURE AND PRINTS################################################
-def testing_model(model, dataloaders):
+def testing_model(model, dataloaders, vid, total_frames, names):
     since = time.time()
     seizing_preds = []
-    inter = []
-    smoothed =[0]
     t = 0
-    idx = 0
-    id = 1
-    frame_bubble = []
     worms_seizing_act = []
     testing_acc = []
     confusion = np.zeros([8, 8])
     running_corrects = 0
-
     for inputs, labels in dataloaders[phase]:
         s = time.time()
         inputs = inputs.to(device)
@@ -141,108 +62,70 @@ def testing_model(model, dataloaders):
         running_corrects += torch.sum(predicts == labels.data)
         preds = predicts.cpu().numpy()
         seizing_preds.append(preds)
-        vis.line(seizing_preds, win='seizing_preds', opts=dict(title= 'Raw_Predictions'))
         worms_seizing_act.append(labels.data.cpu().numpy())
         seizing_preds = np.asarray(seizing_preds)
-
-        def correct_15(idx, mode):
-            if len(mode[:idx]) > half:
-                max_15 = np.argmax(np.bincount(np.squeeze(mode[int(idx-half):int(idx+half+1)])))
-            else:
-                length = len(mode[:idx])
-                max_15 = np.argmax(np.bincount(np.squeeze(mode[int(idx-length):int(idx+half+1)])))
-            return max_15
-        def correct_90(idx, mode):
-            if len(mode[:idx]) > half_num:
-                max_90 = np.argmax(np.bincount(np.squeeze(mode[int(idx-half_num):int(idx+half_num+1)])))
-            else:
-                length = len(mode[:idx])
-                max_90 = np.argmax(np.bincount(np.squeeze(mode[int(idx-length):int(idx+half_num+1)])))
-            return max_90
-        def bubble(idx):
-            if len(inter[:idx]) > bubble_check:
-                max_bub = np.argmax(np.bincount(np.squeeze(inter[int(idx-bubble_check):int(idx+bubble_check+1)])))
-            else:
-                length = len(inter[:idx])
-                max_bub = np.argmax(np.bincount(np.squeeze(inter[int(idx-length):int(idx+bubble_check+1)])))
-            return max_bub
-
-        if len(seizing_preds) > (thresh+half):
-            inter.append(correct_15(idx, seizing_preds))
-            idx+=1
-            vis.line(inter, win='inter_preds', opts=dict(title= 'Intermediate_Predictions'))
-        if len(inter) > bubble_check:
-            bub = bubble(id)
-            if bub >= 1:
-                frame_bubble.append(t)
-                if inter[id] > inter[int(id-1)]:
-                    smoothed.append(inter[int(id-1)])
-                    # print('4')
-                elif inter[id] == inter[int(id-1)]:
-                    smoothed.append(inter[id])
-                    # print('5')
-                elif inter[id] < inter[int(id-1)]:
-                    smoothed.append(correct_90(id, smoothed))
-                    # print('6')
-                elif inter[id] > smoothed[int(id-1)]:
-                    smoothed.append(smoothed[int(id-1)])
-                vis.line(smoothed, win='smoothed_preds', opts=dict(title= 'Smoothed_Predictions'))
-            else:
-                # print(bub)
-                if inter[id] > inter[int(id-1)]:
-                    smoothed.append(correct_90(id, inter))
-                    # print(4, correct_90(id, inter))
-                elif inter[id] != smoothed[int(id-1)]:
-                    if inter[id] > smoothed[int(id-1)]:
-                        smoothed.append(correct_90(id, inter))
-                        # print(5.5, correct_90(idx, inter), inter[id])
-                    elif inter[id] < smoothed[int(id-1)]:
-                        smoothed.append(smoothed[int(id-1)])
-                    # print(5, correct_90(id, inter), inter[id])
-                elif inter[id] < inter[int(id-1)]:
-                    smoothed.append(correct_90(id, inter))
-                    # print(6, correct_90(id, inter))
-                elif inter[id] == smoothed[int(id-1)]:
-                    smoothed.append(inter[id])
-                    # print(7, inter[id])
-                vis.line(smoothed, win='smoothed_preds', opts=dict(title= 'Smoothed_Predictions'))
-            id += 1
-
+        med = signal.medfilt(seizing_preds, 91) #kernal size should be odd
+        confusion[seizing_preds[-1], worms_seizing_act[-1]] += 1
+        vis.line(seizing_preds, win='seizing_preds', opts=dict(title= 'Raw_Predictions'))
+        vis.line(med, win='medfilter_preds', opts=dict(title= 'Medfilter_Predictions'))
+        vis.line(worms_seizing_act, win='worms_seizing_target', opts=dict(title= 'trainedVGG-Target'))
+        vis.heatmap(confusion, win='confusion_matrix', opts=dict(ylabel= 'predicted', xlabel= 'target', colormap= 'Electric'))
         p = time.time() - s
         progress(t, total_frames, status=('predicting ' + str(t)))
         time.sleep(p)
-        vis.line(worms_seizing_act, win='worms_seizing_target', opts=dict(title= 'trainedVGG-Target'))
-        confusion[seizing_preds[-1], worms_seizing_act[-1]] += 1
-        vis.heatmap(confusion, win='confusion_matrix', opts=dict(ylabel= 'predicted', xlabel= 'target', colormap= 'Electric'))
-
     testing_acc = (running_corrects/len(labels.data))
     time_elapsed = time.time() - since
     print('Testing complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     print('Testing_acc:', testing_acc.cpu().numpy())
     total_worms_seizing = max(smoothed)
-    total_seizing_preds = len(seizing_preds)
-    total_intermediate = len(inter)
-    total_smoothed = len(smoothed)
-    avg_seizing_preds = (sum(seizing_preds)/(total_seizing_preds/total_frames))/total_worms_seizing
-    avg_intermediate = (sum(inter)/(total_intermediate/total_frames))/total_worms_seizing
-    avg_smoothed = (sum(smoothed)/(total_smoothed/total_frames))/total_worms_seizing
-    avg = ((avg_seizing_preds+avg_intermediate+avg_smoothed)/3)/30
+    total_worms_seizing = max(med)
+    avg_med = ((sum(med)/(len(med)/total_frames))/total_worms_seizing)/fps
     print('REPORT: ')
-    print('Video Analyzed: ', (PATH + phase))
+    print('Video Analyzed: ', vid)
     print('Total Worms Seizing: ', total_worms_seizing)
-    print('Average time Seizing: ', avg + ' seconds')
-    print('Analysis Complete!')
+    print('Average time Seizing: ', avg_med + ' seconds')
+    with open('test.csv', 'wb') as csvfile:
+        s = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        s.writerow('Ground Truth:', worms_seizing_act)
+        s.writerow('Predicted:', med)
+        s.writerow('Confusion Matrix:', confusion)
+        s.writerow('Accuracy:', testing_acc.cpu().numpy())
+        s.writerow('Predicted Average Time Seizing:', avg_med)
+        s.writerow('Video Analyzed:', vid)
+    if vid != names[-1]:
+        print('Initializing Next Video...')
+    else:
+        print('Analysis Complete!')
     return test_model, testing_acc
 
-##INITIALIZE AND RESHAPE MODEL################################################
+##INITIALIZE AND RESHAPE MODEL##################################################
 def initialize_model(PATH):
     test_model= torch.load(PATH)
     test_model.eval()
+    # Initialize the test_modelfor this run
+    test_model = initialize_model(PATH)
+    #Send the model to GPU
+    test_model= test_model.to(device)
     return test_model
 
-# Initialize the test_modelfor this run
-test_model = initialize_model(PATH)
-#Send the model to GPU
-test_model= test_model.to(device)
-#Run test model
-test_model, testing_acc = testing_model(testing_model, dataloaders_dict)
+#Load Data and RUN##############################################################
+names = os.listdir(vid_folder)
+for vid in names:
+    split = vid.split('.')
+    tf = split[-1]
+    phase = str(vid)
+    data_transforms = {
+        phase: transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    root = vid_folder + phase + '/'
+    image_datasets = {x: datasets.ImageFolder(os.path.join(root), data_transforms[x]) for x in [phase]}
+    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=False, num_workers=4) for x in [phase]}
+    print("Initializing Datasets and Dataloaders...")
+    #Run test model
+    test_model = initialize_model(PATH)
+    test_model, testing_acc = testing_model(testing_model, dataloaders_dict, vid, tf, names)
